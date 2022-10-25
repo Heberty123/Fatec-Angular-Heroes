4da50d87b9e851e91013fd959ed96b7845eb0308
Heberty <hebertyprado22005@gmail.com>
2022-10-25 12:51:53 -0300

    added component services and class injectable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +10,8 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  //heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
   hero: Hero = {
@@ -17,9 +19,22 @@ export class HeroesComponent implements OnInit {
     name: 'widstorm'
   };
 
-  constructor() { }
+  //constructor() { }
+  constructor(private heroService: HeroService) {}
 
+  /*
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }*/
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  //ngOnInit(): void {}
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
